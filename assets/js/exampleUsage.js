1fc68f8e51f47129bb28251bffcfc599fab59dba
Zac <zacharymollica@gmail.com>
2019-12-16 11:51:22 -0400

    remove MR test site

--- assets/js/exampleUsage.js
+++
@@ -1,1 +0,0 @@
-var configProfile={profile:{screenName:"musiqueroyale"},domId:"exampleProfile",maxTweets:8,enableLinks:!0,showUser:!0,showTime:!1,showImages:!0,lang:"en"};twitterFetcher.fetch(configProfile);
