b67bd49a6ede7ccab9fae78dc9b3d86f9eaf3e31
Zac <zacharymollica@gmail.com>
2020-11-10 08:55:38 +0000

    start

---
+++ assets/js/exampleUsage.js
@@ -0,0 +1,11 @@
+var configProfile = {
+  "profile": {"screenName": 'musiqueroyale'},
+  "domId": 'exampleProfile',
+  "maxTweets": 8,
+  "enableLinks": true,
+  "showUser": true,
+  "showTime": false,
+  "showImages": true,
+  "lang": 'en'
+};
+twitterFetcher.fetch(configProfile);
